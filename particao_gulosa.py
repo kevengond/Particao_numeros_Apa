3f572c6806cf50653a68b1893db67cec898030d6
Keven Lucas <108683443+kevengond@users.noreply.github.com>
2024-01-03 15:58:09 -0300

    algoritmo guloso

---
+++ particao_gulosa.py
@@ -0,0 +1,45 @@
+import random
+
+
+def two_way_number_partitioning(V):
+  """
+  Resolve o problema da partição de números usando um algoritmo guloso.
+
+  Args:
+    V: Um conjunto de números inteiros positivos.
+
+  Returns:
+    A solução para o problema, representada por um tuplo (V1, V2), onde V1 e V2
+    são os subconjuntos resultantes.
+  """
+
+  V1 = []
+  V2 = []
+
+  # Adiciona o número mais pequeno de V a um dos subconjuntos.
+
+  n = min(V)
+  V1.append(n)
+  V.remove(n)
+
+  # Adiciona os números restantes a um dos subconjuntos, de modo a minimizar
+  # a diferença das somas dos subconjuntos.
+
+  while V:
+    if sum(V1) > sum(V2):
+      V2.append(V.pop())
+    else:
+      V1.append(V.pop())
+
+  return (V1, V2)
+
+
+V = [i for i in range(1, 101)]
+
+(V1, V2) = two_way_number_partitioning(V)
+difference = len(V1) - len(V2)
+
+
+print(V1, end="\n\n\n")
+print(V2, end="\n\n\n")
+print("|V1|-|V2| = {}".format(abs(difference)))

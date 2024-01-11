bea27d58eada61a9d901cf630df321fbeec67ad0
Keven Lucas <108683443+kevengond@users.noreply.github.com>
2024-01-11 16:10:11 -0300

    Update particao_gulosa.py

--- particao_gulosa.py
+++ particao_gulosa.py
@@ -2,36 +2,41 @@ import random
 
 
 def two_way_number_partitioning(V):
-  """
-  Resolve o problema da partição de números usando um algoritmo guloso.
+ """
+ Resolve o problema da partição de números usando um algoritmo guloso.
 
-  Args:
-    V: Um conjunto de números inteiros positivos.
+ Args:
+  V: Um conjunto de números inteiros positivos.
 
-  Returns:
-    A solução para o problema, representada por um tuplo (V1, V2), onde V1 e V2
-    são os subconjuntos resultantes.
-  """
+ Returns:
+  A solução para o problema, representada por um tuplo (V1, V2), onde V1 e V2
+  são os subconjuntos resultantes.
+ """
 
-  V1 = []
-  V2 = []
+ V1 = []
+ V2 = deque()
 
-  # Adiciona o número mais pequeno de V a um dos subconjuntos.
+ # Adiciona o número mais pequeno de V a um dos subconjuntos.
 
-  n = min(V)
-  V1.append(n)
-  V.remove(n)
+ n = min(V)
+ V1.append(n)
+ V.remove(n)
 
-  # Adiciona os números restantes a um dos subconjuntos, de modo a minimizar
-  # a diferença das somas dos subconjuntos.
+ # Adiciona os números restantes a um dos subconjuntos, de modo a minimizar
+ # a diferença das somas dos subconjuntos.
 
-  while V:
-    if sum(V1) > sum(V2):
-      V2.append(V.pop())
-    else:
-      V1.append(V.pop())
+ while V:
+  if sum(V1) > sum(V2):
+   V2.append(V.pop())
+  else:
+   V1.append(V.pop())
 
-  return (V1, V2)
+ # Adiciona um mecanismo de corte
+
+ if abs(sum(V1) - sum(V2)) <= 1:
+  break
+
+ return (V1, V2)
 
 
 V = [i for i in range(1, 101)]
